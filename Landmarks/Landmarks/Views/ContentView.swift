e878892691bafd3c9d46dea49c80d35c8a144925
Oleh Chobotar <oleh.chobotar@bd.com>
2023-02-09 23:53:01 +0200

    Init

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Oleh Chobotar on 14.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
